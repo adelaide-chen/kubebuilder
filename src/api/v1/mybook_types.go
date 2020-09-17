3819ca7c05808ab6ce98f7af8ac611432e5c9be4
adelaide-chen <ada.chen@berkeley.edu>
2020-09-16 19:52:06 -0700

    initial setup

---
+++ src/api/v1/mybook_types.go
@@ -0,0 +1,72 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MyBookSpec defines the desired state of MyBook
+type MyBookSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of MyBook. Edit MyBook_types.go to remove/update
+	ID     string `bson:"_id,omitempty"`
+	Name   string `bson:"name,omitempty"`
+	Author string `bson:"author,omitempty"`
+	ISBN   string `bson:"ISBN,omitempty"`
+	Genre  string `bson:"genre,omitempty"`
+}
+
+// MyBookStatus defines the observed state of MyBook
+type MyBookStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Coll *mongo.Collection
+	Ctx  context.Context
+}
+
+// +kubebuilder:object:root=true
+
+// MyBook is the Schema for the mybooks API
+type MyBook struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MyBookSpec   `json:"spec,omitempty"`
+	Status MyBookStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MyBookList contains a list of MyBook
+type MyBookList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MyBook `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MyBook{}, &MyBookList{})
+}
